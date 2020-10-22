fdc79c312277cc25dd64214f8fc9a2de16101f52
Hiter Harris <hiterharris@comcast.net>
2020-10-22 16:42:00 -0400

    set up files

---
+++ components/Timer.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
+
+const Timer = () => {
+    return (
+    <View styles={styles.timer}>
+        <Text>Timer</Text>
+    </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    timer: {
+
+    }
+});
+
+export default Timer;
